edfbad8a29d9ced70a3f867b86f23febb6b691ed
spacedgatito <miluska.gonzalezar@gmail.com>
2018-10-15 03:14:10 -0500

    Updates

---
+++ precache-manifest.d5d28c4885281c03a5aeb25920ac1c87.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d975ffd74dd0f719d3",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/runtime~main.45d975ff.js"
+  },
+  {
+    "revision": "2b78a0ca5dcb75258399",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/main.2b78a0ca.chunk.js"
+  },
+  {
+    "revision": "cb24c67d3e8c66265a96",
+    "url": "/lim-2018-01-lab-songs-frameworks/static/js/1.cb24c67d.chunk.js"
+  },
+  {
+    "revision": "5d738da982d7c887b90a2052da7f4e21",
+    "url": "/lim-2018-01-lab-songs-frameworks/index.html"
+  }
+];
